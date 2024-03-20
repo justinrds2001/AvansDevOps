7b101750a6f6f0a30669f496df80f9f1828ad30b
TsDiren21 <diren_2001@hotmail.com>
2024-03-20 16:36:29 +0100

    Base for sprint state, factory state, observer and visitor added

---
+++ AvansDevOps/SprintState/SprintState.cs
@@ -0,0 +1,35 @@
+﻿using AvansDevOps.ISprintFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansDevOps.SprintState
+{
+    public abstract class SprintState
+    {
+        public Sprint Sprint { get; set; } = null!;
+
+        virtual public void StartSprint()
+        {
+            throw new InvalidOperationException("You cannot start the sprint!");
+        }
+
+        virtual public void CancelSprint()
+        {
+            throw new InvalidOperationException("You cannot cancel the sprint!");
+        }
+
+        virtual public void FinishSprint()
+        {
+            throw new InvalidOperationException("You cannot finish the sprint!");
+        }
+
+        virtual public void CloseSprint()
+        {
+            throw new InvalidOperationException("You cannot close the sprint!");
+        }   
+
+    }
+}
