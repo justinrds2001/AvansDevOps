455d94ed520593f070904b09411fa385f278896a
TsDiren21 <diren_2001@hotmail.com>
2024-03-24 22:19:01 +0100

    added forum and project tests

---
+++ AvansTestOpps/ForumTests.cs
@@ -0,0 +1,36 @@
+﻿using AvansDevOps.Composite;
+using AvansDevOps.Observer;
+using AvansDevOps.Observer.Users;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansTestOpps
+{
+    public class ForumTests
+    {
+        [Fact]
+        public void AddThread_Should_Add_Thread_And_Notify_Subscribers()
+        {
+            // Arrange
+            var forum = new Forum();
+            var subscriber1 = new Mock<ISubscriber>();
+            var subscriber2 = new Mock<ISubscriber>();
+            var thread = new DiscussionThread(new Developer());
+
+            forum.Subscribe(subscriber1.Object);
+            forum.Subscribe(subscriber2.Object);
+
+            // Act
+            forum.AddThread(thread);
+
+            // Assert
+            subscriber1.Verify(s => s.Update("New discussion has started! " + thread.Title), Times.Once);
+            subscriber2.Verify(s => s.Update("New discussion has started! " + thread.Title), Times.Once);
+            Assert.Contains(thread, forum.Threads);
+        }
+    }
+}
---
+++ AvansTestOpps/ProjectTests.cs
@@ -0,0 +1,42 @@
+﻿using AvansDevOps.Pipeline;
+using AvansDevOps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansTestOpps
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void AddPipeline_Should_Add_Pipeline()
+        {
+            // Arrange
+            var project = new Project();
+            var pipeline = new Pipeline();
+
+            // Act
+            project.AddPipeline(pipeline);
+
+            // Assert
+            Assert.Contains(pipeline, project.Pipelines);
+        }
+
+        [Fact]
+        public void Project_Initialization_Should_Have_Empty_Collections()
+        {
+            // Arrange & Act
+            var project = new Project();
+
+            // Assert
+            Assert.Empty(project.Name);
+            Assert.Empty(project.Backlog.BacklogItems);
+            Assert.Empty(project.Pipelines);
+            Assert.Empty(project.Sprints);
+            Assert.Empty(project.Repositories);
+            Assert.Empty(project.Participants);
+        }
+    }
+}
